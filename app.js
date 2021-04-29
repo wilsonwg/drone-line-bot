a36c205513026a1c58c55986bdfe016b90d5380a
Wilson Wang <wilsonwg@gmail.com>
2021-04-29 21:04:36 +0800

    read env

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const Client = require('@line/bot-sdk').Client;
+require('dotenv').config();
 
 const client = new Client({
     channelAccessToken: 'n8Bl860OTH6sGiOgre2HtWUG0XhzATy+7REev7j9JyAhjjm71XomzPISmKEfKP7blYTL/bLAU8dNepuR0s3r19tq+Gjg5dW+/MPZm15dFgDZQWS2rYeCiuM0b/cm18Y77A9iREWgLTDYYCoixkWFkwdB04t89/1O/w1cDnyilFU=',
@@ -12,8 +13,18 @@ const client = new Client({
 //   .then(data => console.log(data))
 //   .catch(e => console.log(e));
 
-  client.broadcast({
-    type: 'text',
-    text: '測試訊息',
-  })
-  .catch(e => console.log(e));
+//   client.broadcast({
+//     type: 'text',
+//     text: '測試訊息',
+//   })
+//   .catch(e => console.log(e));
+
+
+if (process.env.DRONE_BUILD_STATUS && process.env.DRONE_BUILD_STATUS != null){
+    client.broadcast({
+        type: 'text',
+        text: `[${process.env.DRONE_BUILD_STATUS}] (${process.env.DRONE_COMMIT_BRANCH}) "${process.env.DRONE_COMMIT_MESSAGE}" by ${process.env.DRONE_COMMIT_AUTHOR}`,
+      })
+      .catch(e => console.log(e));
+}
+
